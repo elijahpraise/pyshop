1eb04dd9d4c588ef731d85d26e1836d2c987d087
elijahpraise <elijahpraise21@gmail.com>
2021-08-29 04:49:47 +0200

    code

--- projectDjango/settings.py
+++ projectDjango/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 # Application definition
 
